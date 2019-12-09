a9fd9fe5373d01c4f9ad7e2426765c45b7610501
Ronald Jerez <dlanor@gmail.com>
2019-12-09 01:12:41 -0500

    refactor: make dynamic mask logic simpler to understand

--- src/dynamic-mask.js
+++ src/dynamic-mask.js
@@ -2,18 +2,18 @@ import maskit from './maskit'
 
 export default function dynamicMask(value, config = {}) {
   const masks = config.masks.slice().sort((a, b) => a.length - b.length)
+  const withConfig = (overrides) => Object.assign({}, config, overrides)
+
+  const nextMaskIsLarger = (currentMask, nextMask) => {
+    return maskit(value, withConfig({ mask: nextMask, short: true })).length > currentMask.length
+  }
 
   for (let i = 0; i < masks.length; i++) {
     const currentMask = masks[i]
     const nextMask = masks[i + 1]
 
-    if (
-      !(
-        nextMask &&
-        maskit(value, Object.assign({}, config, { mask: nextMask, short: true })).length > currentMask.length
-      )
-    ) {
-      return maskit(value, Object.assign(config, { mask: currentMask }))
+    if (!nextMask || !nextMaskIsLarger(currentMask, nextMask)) {
+      return maskit(value, withConfig({ mask: currentMask }))
     }
   }
 
