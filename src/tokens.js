a5d2ca1a469c756632de56735599066e702eb843
Ronald Jerez <dlanor@gmail.com>
2019-12-27 00:18:21 -0500

    refactor(tokens): added case insensitive tag, removed deprecated escape char

--- src/tokens.js
+++ src/tokens.js
@@ -1,9 +1,8 @@
 export default {
   '#': { pattern: /\d/ },
-  X: { pattern: /[0-9a-zA-Z]/ },
-  S: { pattern: /[a-zA-Z]/ },
-  A: { pattern: /[a-zA-Z]/, transform: (v) => v.toLocaleUpperCase() },
-  a: { pattern: /[a-zA-Z]/, transform: (v) => v.toLocaleLowerCase() },
-  '!': { escape: true }, // deprecated, use \ moving forward
+  X: { pattern: /[0-9a-z]/i },
+  S: { pattern: /[a-z]/i },
+  A: { pattern: /[a-z]/i, transform: (v) => v.toLocaleUpperCase() },
+  a: { pattern: /[a-z]/i, transform: (v) => v.toLocaleLowerCase() },
   '\\': { escape: true }
 }
